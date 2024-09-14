73751c126d526905edf26b2563e0180f93ac7c65
gad.maj <gadymajor@gmail.com>
2024-09-13 17:58:13 -0700

    profs

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-var spinText = document.getElementById("spinText");
-        var isSpinning = true;
+document.addEventListener("DOMContentLoaded", function () {
+    var spinText = document.getElementById("spinText");
+    var isSpinning = true;
 
-        function toggleSpin() {
-            if (isSpinning) {
-                spinText.style.animationPlayState = "paused";
-                isSpinning = false;
-            } else {
-                spinText.style.animationPlayState = "running";
-                isSpinning = true;
-            }
+    function toggleSpin() {
+        if (isSpinning) {
+            spinText.style.animationPlayState = "paused";
+            isSpinning = false;
+        } else {
+            spinText.style.animationPlayState = "running";
+            isSpinning = true;
         }
+    }
 
-        spinText.addEventListener("click", toggleSpin);
+    spinText.addEventListener("click", toggleSpin);
+});
