a492b7399fafde903db6bcc29ad45b68dc199e12
Gad Major <gad@digitalpocket.com>
2026-06-22 09:15:36 -0400

    bru

--- scripts/view.js
+++
@@ -1,9 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-        const file = params.get('file');
-        if (file) {
-            document.getElementById('filename').textContent = file;
-            fetch(file)
-                .then(r => r.text())
-                .then(data => document.getElementById('code').textContent = data)
-                .catch(err => document.getElementById('code').textContent = 'Error: ' + err);
-        }
